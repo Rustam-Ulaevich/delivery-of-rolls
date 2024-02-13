b90cb467515205f722f66a9ca1f4ab0e6fdfe395
Rustam-Ulaevich <praporglamur@gmail.com>
2024-02-13 18:13:09 +0500

    Change JavaSript

--- js/counter.js
+++ js/counter.js
@@ -1,14 +1,21 @@
-const btnMinus = document.querySelector('[data-action="minus"]')
-const btnPlus = document.querySelector('[data-action="plus"]')
-let counter = document.querySelector('[data-counter]')
+window.addEventListener("click", function (event) {
+  let counter;
 
-btnMinus.addEventListener('click', function(){
-  if(parseInt(counter.innerText) > 1){
-    counter.innerText = --counter.innerText
+  if (
+    event.target.dataset.action === "plus" ||
+    event.target.dataset.action === "minus"
+  ) {
+    const counterWrapper = event.target.closest(".counter-wrapper");
+    counter = counterWrapper.querySelector("[data-counter]");
   }
-})
-btnPlus.addEventListener('click', function(){
-  counter.innerText = ++counter.innerText
-})
 
-console.log(counter);
+  //функция складывания счётчика
+  if (event.target.dataset.action === "plus") {
+    counter.innerText = ++counter.innerText;
+  }
+  if (event.target.dataset.action === "minus") {
+    if (parseInt(counter.innerText) > 1) {
+      counter.innerText = --counter.innerText;
+    }
+  }
+});
